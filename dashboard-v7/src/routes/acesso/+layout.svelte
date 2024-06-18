203bc366bb7eb45f0b75167379d8089bafeab1ba
Fred Pinheiro <fredvpg@gmail.com>
2024-06-18 17:01:11 -0300

    .

--- dashboard-v7/src/routes/acesso/+layout.svelte
+++ dashboard-v7/src/routes/acesso/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import Layout from '../../components/dashboard/Layout.svelte';
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
 	/** @type {import('./$types').LayoutData} */
 	export let data;
-	// console.log(data);
+	const condos = writable();
+	$: condos.set(data.condominios);
+	setContext('condos', condos);
 </script>
 
 <Layout bind:userData={data.userData}>
